a68a00b33412046b27f62ebbe568e0ec2ba25b21
okoth kongo <okothkongo@gmail.com>
2020-03-28 15:37:26 +0300

    rectify failing cops

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+      params.require(:user).permit(:email, :password, :password_confirmation,
+                                   :first_name, :last_name)
     end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! user, :id, :email, :password, :password_confirmation, :first_name, :last_name, :created_at, :updated_at
+json.extract! user, :id, :email, :password, :password_confirmation, :first_name, 
+                    :last_name, :created_at, :updated_at
 json.url user_url(user, format: :json)
