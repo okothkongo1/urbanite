45fe6d09d000c88720256e41aa81af4522ef82b5
okoth kongo <okothkongo@gmail.com>
2020-03-31 13:10:12 +0300

    user view cleaner up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   NAME_REGEX = /(\A\D{1,30}\Z)/
   validates :first_name, presence: true, format: { with: NAME_REGEX,
-    message: 'format is invalid' }
+            message: 'format is invalid', allow_blank: true }
   validates :last_name, presence: true, format: { with: NAME_REGEX,
-    message: 'format is invalid' }
+            message: 'format is invalid',allow_blank: true }
   validates :email, presence: true, format: { with: EMAIL_REGEX,
-    message: 'format is invalid' },uniqueness:  { case_sensitive: false }
+            message: 'format is invalid', allow_blank: true },
+            uniqueness:  { case_sensitive: false }
   validates :password, presence: true,format: { with: PASSWORD_REGEX,
-    message: 'is too weak' }, confirmation: true
-  validates :password_confirmation, presence: true
+            message: 'is too weak', allow_blank: true },
+            confirmation: true
 end
