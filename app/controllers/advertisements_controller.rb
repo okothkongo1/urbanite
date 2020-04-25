87c43c0c4410e11ec11f5e663780552aa6a7e195
okoth kongo <okothkongo@gmail.com>
2020-04-25 10:02:43 +0300

    advert crud

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,28 +1,46 @@
 class AdvertisementsController < ApplicationController
-  before_action :authenticate_user!, :set_advert, only: [:show]
+  before_action :authenticate_user!, :set_advert, only: [:show, :destroy, :update,:edit]
     def index
     @advertisetments = Advertisement.all.where(user_id: current_user.id)
     end
 
-  def show
-  end
-  def new
-    @advertisement = Advertisement.new
-  end
+    def show
+    end
+    def new
+      @advertisement = Advertisement.new
+    end
+    def edit
+    end
 
-  def create
-    @advertisement = Advertisement.new(advertisement_params)
+    def create
+      @advertisement = Advertisement.new(advertisement_params)
 
-    respond_to do |format|
-      if @advertisement.save
-        format.html do
-          redirect_to @advertisement, notice: 'advertisement was successfully created.'
+      respond_to do |format|
+        if @advertisement.save
+          format.html do
+            redirect_to @advertisement, notice: 'advertisement was successfully created.'
+          end
+        else
+          format.html { render :new }
         end
-      else
-        format.html { render :new }
       end
     end
-  end
+    def update
+        respond_to do |format|
+          if @high_score.update(high_score_params)
+            format.html { redirect_to @advertisement, notice: 'advert was successfully updated'}
+          else
+            format.html { render :edit }
+          end
+        end
+    end
+    def destroy
+      @advertisement.destroy
+      respond_to do |format|
+        format.html { redirect_to advertisements_url, notice: 'advert was successfully deleted.' }
+      end
+
+    end
 
   private
     def advertisement_params
