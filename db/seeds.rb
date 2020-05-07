04a117d61b140a37bd5b984acfeba823640989a5
okoth kongo <okothkongo@gmail.com>
2020-05-07 20:52:53 +0300

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+100.times do
+  User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.free_email,
+      password: 'VeryStrong@123',
+      password_confirmation:'VeryStrong@123',
+      confirmed_at: Time.current
+  )
+end
