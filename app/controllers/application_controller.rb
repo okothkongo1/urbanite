913a6d49c3de2fe405ac7ffe0468050df145fd49
okoth kongo <okothkongo@gmail.com>
2020-05-17 23:30:34 +0300

    redirect admin to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+  private
+  def after_sign_in_path_for(_resource)
+    if current_user.admin?
+      admin_users_path
+    else
+      root_path
+    end
+  end
 end
