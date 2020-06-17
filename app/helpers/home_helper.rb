c3f6a64a8035bc2e0cc5edb39a12a010c34f49f8
okothkongo <okothkongo@gmail.com>
2020-06-17 17:52:42 +0300

    restructure home-page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 module HomeHelper
   def categories
-    {'Electronics': 'important_devices'}
+    {'Electronics': 'radio', 'Health': 'local_hospital',
+      'Home and Offices': '', 'Computing': 'computer',
+      'Baby Products': 'child_care', 'Sports&Exercise': 'sports_soccer',
+      'Food & Agriculture': 'food_bank', 'Services': 'room_service',
+      'Real Estate': 'apartment', 'Vehicles': 'drive_eta',
+      'Jobs': 'work', 'Construction&Repair': 'construction',
+       'Mobile&Tablets': 'smartphone'
+    }
+  end
+  def place_holder_helper
+    (1..12).to_a
   end
 
 end
